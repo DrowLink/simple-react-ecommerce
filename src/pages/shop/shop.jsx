d4177c7ba1b21ff191c9658d00b490884395fa59
drowlink <pauloriveiro01@gmail.com>
2023-07-26 15:38:44 -0400

    done

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { PRODUCTS } from "../../products.js"
-import { Product } from './product.jsx'
-import "./shop.css"
+import React from "react";
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
+import "./shop.css";
 
 export const Shop = () => {
   return (
-    <div className='shop'>
-        <div className='shopTitle'>
-            <h1> DrowLink Shop </h1>
-        </div>
-        <div className="products">
-            {PRODUCTS.map( (products) => { 
-                return <Product data={products}/>
-            })}
-        </div>
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>PedroTech Shop</h1>
+      </div>
+
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
